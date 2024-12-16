a59a94a9ebfd8efa5316ca8eafe70d7289f6b1f8
Clément Rautureau <clement.rautureau@kshuttle.io>
2024-12-16 15:35:23 +0100

    Better but false Day16 part 2

--- day16_2.py
+++ day16_2.py
@@ -6,6 +6,7 @@ dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 start = None
 end = None
 
+size = (17, 17)
 size = (141, 141)
 
 with open("input.txt", "r", encoding="utf-8") as fichier:
@@ -30,7 +31,7 @@ for key, path in paths.items():
         if pos in paths.keys():
             path['side'].append(pos)
 
-def find(cases, all):
+def find(cases):
     next = []
     for case in cases:
         path = paths[case['path']]
@@ -52,8 +53,44 @@ def find(cases, all):
             })
     if len(next) == 0:
         return
-    all.extend(next)
-    find(next, all)
+    find(next)
     
+def getPath(cases, set):
+    next = []
+    for n in cases:
+        if not n in set:
+            set.append(n)
+    for case in cases:
+        path = paths[case]
+        for side in path['side']:
+            if side in set:
+                continue
+            sideObj = paths[side]
+            diff = path['value'] - sideObj['value']
+            if diff < 0 and (diff != -999 or case == end):
+                continue
+            if not side in next:
+                next.append(side)
+    
+    if len(next) == 0:
+        return set
+    
+    return getPath(next, set)
         
-find([{'path': start, 'dir' : (0, 1), 'value': 0}], [])
+        
+find([{'path': start, 'dir' : (0, 1), 'value': 0}])
+
+res = getPath([end], [])
+
+map = [["#" for _ in range(size[0])] for _ in range(size[1])]
+
+for p in paths.keys():
+    map[p[0]][p[1]] = ' '
+for r in res:
+    map[r[0]][r[1]] = 'o'
+    
+    
+for line in map:
+    print("".join(line))
+    
+print(len(res))
